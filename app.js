a08845f7e8092afd8cd4b9f059bdd827c3d7a27e
karyukawamura <karyukawamura@gmail.com>
2019-09-06 09:07:05 +0800

    meow

--- app.js
+++ app.js
@@ -11,27 +11,35 @@ app.engine("handlebars", exphb({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.get("/", function(req, res) {
-  res.render("main");
+  //connecting db to node
+  var { Client } = require("pg");
+  var client = new Client({
+    users: "noelwat",
+    password: "noel123",
+    port: 5432,
+    database: "exercise"
+  });
+
+  client
+    .connect()
+    .then(() => console.log("connected to DB"))
+    .then(() =>
+      client.query("select * from clothesfemale where style_id = $1", [1])
+    )
+    .then(results => {
+      console.table(results.rows);
+      res.render("main", { clothes: results.rows });
+    })
+
+    .catch(e => console.log(e))
+    .finally(() => client.end());
 });
 
-//connecting db to node
-var { Client } = require("pg");
-var client = new Client({
-  users: "noelwat",
-  password: "noel123",
-  port: 5432,
-  database: "exercise"
-});
-
-client
-  .connect()
-  .then(() => console.log("connected to DB"))
-  .then(() =>
-    client.query("select * from clothesmale where style_id = $1", [2])
-  )
-  .then(results => console.table(results.rows))
-  .catch(e => console.log(e))
-  .finally(() => client.end());
+//   function createdHandlebars(malePop)
+// {
+//   var rawtemplate = $("card-img-top")
+//   var
+// }
 
 // app.get("/hat", (req, res) => {
 //   client
--- migrations/20190904110247_lookBookTables.js
+++
@@ -1,8 +0,0 @@
-exports.up = function(knex) {
-  return knex.schema.createTable(horoscopes, function(table) {
-    table.increments();
-    table.string("signs").notnullable();
-  });
-};
-
-exports.down = function(knex) {};
