25e0904b4b557a06b6fb773510475d0fa81eba56
karyukawamura <karyukawamura@gmail.com>
2019-09-06 09:52:44 +0800

    commiting

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require("express");
 var bodyParser = require("body-parser");
 var port = process.env.PORT || 8080;
@@ -60,41 +59,3 @@ app.get("/", function(req, res) {
 app.listen(port, function() {
   console.log(`Listening to Port: `, port);
 });
-=======
-const express = require('express');
-const app = express();
-const session = require('express-session');
-const setupPassport = require('./passport/passport');
-const bodyParser = require('body-parser');
-const router = require('./router')(express);
-const morgan = require('morgan');
-const hb = require('express-handlebars');
-
-app.engine('handlebars', hb({
-  defaultLayout: 'main'
-}));
-
-app.set('view engine', 'handlebars');
-
-app.use(morgan('combined'));
-
-app.use(session({
-  secret: 'supersecret',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {secure: false}
-}));
-
-app.use(express.static('public'))
-// app.use(express.static('pages'))
-
-app.use(bodyParser());
-
-setupPassport(app);
-
-app.use('/', router);
-
-app.listen(8080, function () {
-  console.log(`Application is listening to port 8080`)
-});
->>>>>>> 90bc63d76a40c03cecc16c7af37379d12dfb4b6d
